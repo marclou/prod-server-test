f2b6cc74b76275e68920b6cfa4498d1d6a943b73
marclou <marc.louvion@gmail.com>
2018-03-18 17:23:07 +0900

    adding test files

--- server/server.test.js
+++ server/server.test.js
@@ -3,15 +3,18 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('Should return my name and a status that is 200', (done) => {
-     request(app)
-          .get('/users')
-          .expect(200)
-          .expect((res) => {
-               expect(res.body).toInclude({
-                    name: 'Marc',
-                    age: 24
-               });
-          })
-          .end(done);
+describe('Server', () => {
+     it('Should return my name and a status that is 200', (done) => {
+          request(app)
+               .get('/users')
+               .expect(200)
+               .expect((res) => {
+                    expect(res.body).toInclude({
+                         name: 'Marc',
+                         age: 24
+                    });
+               })
+               .end(done);
+     });
+
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,19 +2,19 @@ const expect = require('expect');
 
 const utils = require('./utils.js');
 
-it('Should add two numbers', () =>  {
-     const test = utils.add(3, 112);
+describe('Utils', () => {
+     describe('#add', () => {
+          it('Should add two numbers', () =>  {
+               const test = utils.add(3, 112);
 
-     expect(test).toBe(115).toBeA('number');
-});
+               expect(test).toBe(115).toBeA('number');
+          });
+     });
 
-it('Should contains string lastname and first name', () => {
-     const user = utils.setName({}, 'Marc Louvion');
+     it('Should contains string lastname and first name', () => {
+          const user = utils.setName({}, 'Marc Louvion');
 
-     expect(user.firstName).toExist().toBeA('string');
-     expect(user.lastName).toExist().toBeA('string');
-});
-
-it('it should asyn add 2 numbers', () => {
-     
+          expect(user.firstName).toExist().toBeA('string');
+          expect(user.lastName).toExist().toBeA('string');
+     });
 });
