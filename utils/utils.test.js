672955f727d16933dd63a97131ae3224f73f2eba
marclou <marc.louvion@gmail.com>
2018-03-18 14:55:21 +0900

    adding testing via mocha module

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,9 +1,9 @@
+const expect = require('expect');
+
 const utils = require('./utils.js');
 
 it('Should add two numbers', () =>  {
      const test = utils.add(3, 112);
 
-     if (test !== 115) {
-          throw new Error(`Should be 115, but got ${test} instead..`);
-     }
+     expect(test).toBe(115).toBeA('number');
 });
